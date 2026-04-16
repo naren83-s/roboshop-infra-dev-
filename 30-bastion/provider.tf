ab3df4080e298d19ea8dfe16134ec7f6cffd5659
narendrakumar-sun <narendra.sunkara83@gmail.com>
2026-04-16 17:29:02 +0400

    action

--- 30-bastion/provider.tf
+++ 30-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
    backend "s3" {
     bucket  = "renote-state-naren83-dev" # Replace with your unique bucket name
-    key     = "roboshop-dev-sg-rules"
+    key     = "roboshop-dev-bastion"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
